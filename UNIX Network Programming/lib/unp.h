2c4f156bef846c58a74715662168f53ecb36ab0f
1iqinggui <1iqinggui@sina.com>
2018-10-21 11:46:36 +0800

    UNIX Network Programming

--- UNIX Network Programming/lib/unp.h
+++ UNIX Network Programming/lib/unp.h
@@ -4,58 +4,25 @@
 #ifndef __unp_h
 #define __unp_h
 
-#include <sys/socket.h>		/* basic socket definitions */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <sys/socket.h>	/* basic socket definitions */
 
-#ifdef HAVE_STRINGS_H
-# include <strings.h>		/* for convenience */
-#endif
-
+#include	<errno.h>
+#include 	<fcntl.h>	/* for nonblocking */
+#include	<netdb.h>
+#include	<signal.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
 
 /* Miscellaneous constants */
-#define MAXLINE		4096	/* max text line length */
-#define BUFFSIZE 	8196	/* buffer size for reads and writes */
-
-/* Define some port number that can be used for our example */
-#define SERV_PORT	9877		/* TCP and UDP */
-#define SERV_PORT_STR	"9877"	/* TCP and UDP */
+#define MAXLINE 	4096	/* max text line length */
+#define BUFFSIZE	8192	/* buffer size for reads and writes */
 
 /* Following shortens all the typecasts of pointer arguments: */
 #define SA struct sockaddr
 
-
-
-#define min(a, b)	((a) < (b) ? (a) : (b))
-#define max(a, b)	((a) > (b) ? (a) : (b))
-
-
-
-void dg_cli(FILE *, int, const SA *, socklen_t);
-void dg_echo(int, SA *, socklen_t);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* prototypes for our socket wrapper functions: see {Sec errors} */
+int 	Accpet(int, SA *, socklen_t *);
+void 	Bind(int, const SA *, socklen_t);
+void 	Connect(int, const SA *, socklen_t);
 #endif
